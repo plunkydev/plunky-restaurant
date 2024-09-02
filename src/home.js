aa1c602ec394a1c008d426903000f6888c51aaa3
plunkydev <plunkydesign@gmail.com>
2024-09-02 18:16:03 -0500

    Add funcion para generar el contenido del home

---
+++ src/home.js
@@ -0,0 +1,5 @@
+export default function createHome() {
+    const div = document.createElement("div");
+    div.innerHTML = `<h1>Bienvenidos a Plunky's Restaurant</h1>`;
+    return content.appendChild(div);
+};
