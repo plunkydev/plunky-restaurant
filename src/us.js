98dd01ab553e33ae0ae24c6eb9345227b10a846e
plunkydev <plunkydesign@gmail.com>
2024-09-07 19:22:28 -0500

    Agregado contenido

--- src/us.js
+++ src/us.js
@@ -1,6 +1,15 @@
+import hamburguesa from "./images/hamburguesa.png";
 export default function createUs() {
     const div = document.createElement("div");
-    div.innerHTML = `<h1>About Us</h1>`;
+    div.classList.add('aboutUs');
+    div.innerHTML = `<div class="hero2">
+                    <div class="bannerHero hero-item">
+                        <img src="${hamburguesa}" alt="plato de comida">
+                    </div>
+                    <div class="bienvenida hero-item">
+                        <h2>Plunky's Restaurant</h2>
+                        <p>Sabor con Cariño</p>
+                    </div>`;
     content.innerHTML = "";
     return content.appendChild(div);
 };
