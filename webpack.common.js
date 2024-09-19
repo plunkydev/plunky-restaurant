9793c31b1e6131def2a0a9fd73ab248ce55ffdb3
plunkydev <plunkydesign@gmail.com>
2024-09-18 20:16:05 -0500

    Aplicado modos para produccion y para desarrollo

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,13 @@
-// webpack.config.js
+// webpack.common.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -32,7 +27,7 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
                 type: "asset/resource",
-            }
+            },
         ],
     },
 };
